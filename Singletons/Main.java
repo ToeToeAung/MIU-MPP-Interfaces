5070ff5ddd7dd9114f5948ea68600d5cd04fa27e
Toe Toe Aung <toetoeag@gmail.com>
2024-07-16 18:52:42 -0500

    MIU MPP Interfaces - Using Enums to create Singletons

--- Singletons/Main.java
+++ Singletons/Main.java
@@ -4,6 +4,16 @@ public class Main {
     public static void main(String[] args) {
      //MySingleton mytest=new  MySingleton.INSTANCE.behavior();
         //Enum to create a singleton
-        MySingleton.INSTANCE.behavior();
+       // MySingleton.INSTANCE.behavior();
+       MySingleton instance1 = MySingleton.INSTANCE;
+       MySingleton instance2 = MySingleton.INSTANCE;
+
+
+       if(instance1 == instance2){
+           System.out.println("Instances are equal");
+       }else{
+           System.out.println("Instances are not equal");
+       }
+
     }
 }
